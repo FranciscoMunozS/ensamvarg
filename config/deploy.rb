f9357d05057fc286a206de35eba21f5cddff8cc6
FranciscoMunozS <franciscomuniozs@gmail.com>
2018-03-23 15:55:05 -0300

    last update - fixed deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,12 @@
 lock "~> 3.10.1"
 
 set :application, "Ensamvarg"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:FranciscoMunozS/ensamvarg.git"
+
+set :deploy_to, '/home/deploy/Ensamvarg'
+
+append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
